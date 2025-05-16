db0ada671a85988d34e7e240bce14ddc0bdd4c8a
JinYang0318 <jyap384@gmail.com>
2025-05-16 16:04:45 +0800

    commit

---
+++ src/main/java/com/orderflow_common/orderflow_common/OrderflowCommonApplication.java
@@ -0,0 +1,13 @@
+package com.orderflow_common.orderflow_common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderflowCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderflowCommonApplication.class, args);
+	}
+
+}
